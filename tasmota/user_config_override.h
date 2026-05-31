1ecfc443b2ef87bfd13c2a04839b2fbcfeabd439
ufabian1 <145349008+ufabian1@users.noreply.github.com>
2026-05-31 17:19:09 +0200

    Update user_config_override.h

--- tasmota/user_config_override.h
+++ tasmota/user_config_override.h
@@ -11,33 +11,25 @@
 
 #define USE_UFILESYS
 
-// große Module raus
+// große Systeme raus
 #undef USE_ZIGBEE_ZNP
 #undef USE_DISPLAY
 #undef USE_KNX
 #undef USE_TUYA_MCU
 
-// Licht raus
+// Licht/Dimmer raus
 #undef USE_LIGHT
 #undef USE_WS2812
-#undef USE_LIGHT_PALETTE
-#undef USE_SM16716
-#undef USE_SM2135
-#undef USE_MY92X1
-
-// Dimmer raus
-#undef USE_SHUTTER
 #undef USE_PWM_DIMMER
-#undef USE_SONOFF_D1
-#undef USE_SHELLY_DIMMER
-#undef USE_EXS_DIMMER
+#undef USE_SHUTTER
 
-// Sensoren minimieren
+// Sensor stack reduzieren
 #undef USE_DHT
 #undef USE_BME280
 #undef USE_SHT3X
 #undef USE_I2C
 #undef USE_SPI
+#undef USE_ADC
 
 // Integrationen raus
 #undef USE_HOME_ASSISTANT
@@ -45,21 +37,19 @@
 #undef USE_EMULATION_HUE
 #undef USE_EMULATION_WEMO
 
-// weitere Features raus
-#undef USE_DEVICE_GROUPS
-#undef USE_SERIAL_BRIDGE
-#undef USE_BUZZER
+// Netzwerk extras
+#undef USE_DISCOVERY
+#undef USE_WEBSEND_RESPONSE
 
-// Energie raus (du nutzt SML!)
+// Logging reduzieren
+#define SYS_LOG_LEVEL 0
+
+// Energy raus (du nutzt SML!)
 #undef USE_PZEM004T
 #undef USE_PZEM_AC
 #undef USE_PZEM_DC
 #undef USE_MCP39F501
 #undef USE_ADE7953
 #undef USE_BL0940
-
-// Web reduzieren
-#undef USE_WEBSERVER_TLS
-#undef USE_WEBSEND_RESPONSE
 
 #endif
