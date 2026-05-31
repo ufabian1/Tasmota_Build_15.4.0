c95a06973b8501dbabf8a8c1cffbdcf091f50d65
ufabian1 <145349008+ufabian1@users.noreply.github.com>
2026-05-31 17:51:32 +0200

    Update user_config_override.h

--- tasmota/user_config_override.h
+++ tasmota/user_config_override.h
@@ -11,45 +11,27 @@
 
 #define USE_UFILESYS
 
-// große Systeme raus
+// große Module raus
 #undef USE_ZIGBEE_ZNP
 #undef USE_DISPLAY
-#undef USE_KNX
-#undef USE_TUYA_MCU
 
-// Licht/Dimmer raus
+// Licht raus
 #undef USE_LIGHT
 #undef USE_WS2812
-#undef USE_PWM_DIMMER
-#undef USE_SHUTTER
+#undef USE_LIGHT_PALETTE
 
-// Sensor stack reduzieren
-#undef USE_DHT
-#undef USE_BME280
-#undef USE_SHT3X
-#undef USE_I2C
-#undef USE_SPI
-#undef USE_ADC
+// Dimmer raus
+#undef USE_SHUTTER
+#undef USE_PWM_DIMMER
+
+// Energy raus
+#undef USE_PZEM004T
+#undef USE_PZEM_AC
+#undef USE_ADE7953
+#undef USE_BL0940
 
 // Integrationen raus
 #undef USE_HOME_ASSISTANT
 #undef USE_DOMOTICZ
-#undef USE_EMULATION_HUE
-#undef USE_EMULATION_WEMO
-
-// Netzwerk extras
-#undef USE_DISCOVERY
-#undef USE_WEBSEND_RESPONSE
-
-// Logging reduzieren
-#define SYS_LOG_LEVEL 0
-
-// Energy raus (du nutzt SML!)
-#undef USE_PZEM004T
-#undef USE_PZEM_AC
-#undef USE_PZEM_DC
-#undef USE_MCP39F501
-#undef USE_ADE7953
-#undef USE_BL0940
 
 #endif
